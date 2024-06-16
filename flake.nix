e642a92d44a23b2b2bb08a91cbebedaeba9d3078
efficacy38 <efficacy38@gmail.com>
2024-06-17 01:41:37 +0800

    tidy: run formatter

--- flake.nix
+++ flake.nix
@@ -14,13 +14,16 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       homeConfigurations."efficacy38" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
