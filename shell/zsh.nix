cda15c82d111ae7663aebfbe2f94cc7d3bdf7232
efficacy38 <efficacy38@gmail.com>
2024-06-17 03:38:10 +0800

    feat(zsh): add custom scripts, alias

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -1,30 +1,155 @@
 { config, pkgs, ... }: {
   programs.zsh = {
     enable = true;
-    initExtra = ''
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-    '';
     plugins = [
-      { name = "zsh-autosuggestions"; src = pkgs.zsh-autosuggestions; }
-      { name = "zsh-syntax-highlighting"; src = pkgs.zsh-syntax-highlighting; }
       { name = "zsh-completions"; src = pkgs.zsh-completions; }
       { name = "powerlevel10k"; src = pkgs.zsh-powerlevel10k; file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme"; }
     ];
+    autosuggestion.enable = true;
+    completionInit = ''
+      autoload -U +X bashcompinit && bashcompinit
+      autoload -U compinit && compinit
+      source <(incus completion zsh)
+
+      # kubectl
+      command -v kubectl > /dev/null && source <(kubectl completion zsh)
+      command -v flux > /dev/null && source <(flux completion zsh)
+      command -v helm > /dev/null && source <(helm completion zsh)
+    '';
+    # default 10000, maybe 1000000 is good for me to use fzf :>
+    history = {
+      save = 1000000;
+      size = 1000000;
+    };
+    syntaxHighlighting = {
+      enable = true;
+      # check highlighters https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
+      highlighters = [
+        "main"
+        "brackets"
+        "pattern"
+        "regexp"
+        "line"
+      ];
+    };
     oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-        plugins =
+      enable = true;
+      theme = "robbyrussell";
+      plugins =
         [
-            "git"
-            "npm"
-            "history"
-            "node"
-            "rust"
-            "deno"
+          "git"
+          "npm"
+          "history"
+          "node"
+          "rust"
+          "deno"
         ];
     };
     profileExtra = ''
       export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
+    '';
+    initExtra = ''
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      # ------------------------ self defined functions ----------------------------
+      # function to create a new tmux session with the name of the current directory
+      function tat {
+          name=$(basename $(pwd) | sed -e 's/\.//g')
+          if [ $(pwd) = $HOME ]; then
+              name="default"
+          fi
+      
+      
+          if ! tmux ls 2>&1 | grep -E "^$name:"; then
+              if [ -f .envrc ]; then
+                  direnv exec / tmux new-session -s "$name" -d
+              else
+                  # start tmux at detached mode
+                  tmux new-session -s "$name" -d
+              fi
+          fi
+      
+          if [ -z "$TMUX" ]; then
+              tmux attach -t "$name" -c "$(pwd)"
+          else
+              tmux switch-client -t "$name"
+          fi
+      }
+      
+      # attach to lxd container with username 
+      function goincus(){
+          if [[ $# -eq 0 ]]; then
+              echo "Usage: $0 <container or vm name> [namespace] [username]" >&2
+              echo ""
+              echo "attach to lxd container with uid 1000"
+              echo "    namespace: default is current namespace"
+              echo "    uid: default is 1000" >&2
+              return 1;
+          fi
+          instance="$1"; shift;
+          project="";
+          uid="efficacy38";
+      
+          if [[ $# -gt 0 ]]; then
+              project=$1; shift;
+          fi
+      
+          if [[ $# -gt 0 ]]; then
+              uid=$1; shift;
+          fi
+      
+          if [[ -z "$project" ]]; then
+              echo "\nenter the $instance at current project\n"
+              incus exec $instance -- sudo -iu "$uid"
+          else
+              echo "\nenter the $instance at project $project\n"
+              incus exec --project $project $instance -- sudo -iu "$uid"
+          fi
+      }
+      
+      # ssh-agent
+      function add-ssh-key() {
+      for key in ~/.ssh/id_ed25519 ~/.ssh/id_sysadm; do
+          # add ssh-key to ssh-agent if not yet added into
+      	if [[ -f "$key" ]]; then
+      	 	ssh-add "$key"
+      	fi
+      done
+      }
+
+      # $1: alias
+      # $2: command
+      function aliasIfExist(){
+          command_base="$(echo $2 | cut -d' ' -f1)"
+          if command -v "$command_base" >/dev/null; then
+              alias $1=$2
+          fi
+      }
+      # ---------------------------- alias section ---------------------------
+      # short cut alias
+      aliasIfExist cat bat
+      aliasIfExist cat batcat
+      aliasIfExist godev 'incus exec --project default dev -- sudo -iu '
+      aliasIfExist bye 'sudo poweroff'
+      aliasIfExist k kubectl
+      aliasIfExist reboot 'sudo reboot'
+      # alias cscc_work='sudo $HOME/.local/bin/cscc_work'
+      aliasIfExist 'cdt' 'cd ~/Tmp'
+      alias chproj='incus project switch'
+      # aliasIfExist ssh "kitty +kitten ssh"
+      
+      if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+          aliasIfExist 'copy' 'wl-copy'
+      else
+          aliasIfExist 'copy' 'xclip -sel clip'
+      fi
+      
+      # overriding alias
+      aliasIfExist vim nvim
+      aliasIfExist vi vim
+      aliasIfExist docker-compose podman-compose
+      aliasIfExist docker podman
+      alias kreload='kquitapp5 plasmashell; plasmashell --replace &'
+      export PATH=$HOME/.local/bin:$PATH
     '';
   };
 
