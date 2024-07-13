c7236c3cdb61e15f241ed613f2e50689eedf7411
efficacy38 <efficacy38@gmail.com>
2024-07-14 00:34:55 +0800

    fixup! fixup! wip

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -22,7 +22,6 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         -- NOTE: geenral purpose language servers
-        "asm_lsp",
         "asmfmt",
         "lua_ls",
         "stylua",
