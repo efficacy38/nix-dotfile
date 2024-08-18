9440e32ac11eb4077ac6c469606b297257bdcbf8
efficacy38 <efficacy38@gmail.com>
2024-08-18 16:48:11 +0800

    feat: add hw optimize flake to desktop environment

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -16,6 +19,12 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/phoenixton/configuration.nix
+          inputs.nixos-hardware.nixosModules.common-cpu-amd
+          inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+          inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+          inputs.nixos-hardware.nixosModules.common-cpu-amd-raphael-igpu
+          inputs.nixos-hardware.nixosModules.common-pc-ssd
+          inputs.nixos-hardware.nixosModules.common-hidpi
         ];
       };
     };
