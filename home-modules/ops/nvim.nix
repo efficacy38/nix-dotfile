d01a9b119ab36db11e86ca0746c28dfa7df34439
efficacy38 <efficacy38@gmail.com>
2024-10-22 02:22:11 +0800

    feat(nvim): install nvim dep in nix

--- home-modules/ops/nvim.nix
+++ home-modules/ops/nvim.nix
@@ -1,8 +1,46 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     neovim
-    nodejs_22
     yamllint
+    nodejs # copilot
+    terraform-ls
+    pyright
+
+    # based on ./suggested-pkgs.json
+    gopls
+    golangci-lint
+    nodePackages.bash-language-server
+    taplo-lsp
+    marksman
+    selene
+    rust-analyzer
+    yaml-language-server
+    nil
+    nixd
+    shellcheck
+    shfmt
+    ruff
+    ruff-lsp
+    nixfmt-rfc-style
+    terraform-ls
+    clang-tools
+    nodePackages.prettier
+    stylua
+    # based on https://github.com/ray-x/go.nvim#go-binaries-install-and-update
+    go
+    gofumpt
+    gomodifytags
+    gotools
+    delve
+    # golines
+    gomodifytags
+    gotests
+    iferr
+    impl
+    reftools
+    ginkgo
+    richgo
+    govulncheck
   ];
 
   home.sessionVariables = {
