f9f4aff267a191dc342e5a691c2e762dafa2f967
efficacy38 <efficacy38@gmail.com>
2024-12-07 00:27:54 +0800

    feat(git): migrate git config to nix

--- home-modules/programing/git.nix
+++ home-modules/programing/git.nix
@@ -29,5 +29,44 @@
         helper = "cache --timeout 3600";
       };
     };
+
+    includes =
+      let
+        cscc-git-config = {
+          user = {
+            name = "Cai-Sian Jhuang";
+            email = "csjhuang@cs.nctu.edu.tw";
+            signingkey = "~/.ssh/cscc.id_ed25519.pub";
+          };
+          commit.gpgsign = "true";
+          gpg.format = "ssh";
+        };
+        gh-git-config = {
+          user = {
+            name = "efficacy38";
+            email = "efficacy38@gmail.com";
+            signingkey = "~/.ssh/gh.id_ed25519.pub";
+          };
+          commit.gpgsign = "true";
+          gpg.format = "ssh";
+        };
+      in
+      [
+        {
+          condition = "hasconfig:remote.*.url:git@gitlab.cc.cs.nctu.edu.tw:10022/**";
+          contents = cscc-git-config;
+        }
+        {
+          condition = "hasconfig:remote.*.url:https://gitlab.it.cs.nycu.edu.tw/**";
+          contents = cscc-git-config;
+        }
+        {
+          condition = "hasconfig:remote.*.url:https://github.com/**";
+          contents = gh-git-config;
+        }
+        {
+          path = ".config/git/99-local.conf";
+        }
+      ];
   };
 }
