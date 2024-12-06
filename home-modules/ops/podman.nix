1e888ca59fe4afde3f1b5e6e1607b04a839937bf
efficacy38 <efficacy38@gmail.com>
2024-12-06 21:45:21 +0800

    feat(podman): add cscc as mirror

--- home-modules/ops/podman.nix
+++ home-modules/ops/podman.nix
@@ -7,6 +7,13 @@
   home.file.".config/containers/registries.conf".text = ''
     [registries.search]
     registries = ['docker.io', 'quay.io', "gcr.io"]
+
+    # use cscc mirror when available
+    [[registry]]
+      prefix = "docker.io"
+      location = "registry-1.docker.io"
+
+      [[registry.mirror]]
+        location = "docker.cccr.test.cc.cs.nctu.edu.tw"
   '';
-  # etc."containers/policy.json".text = import ./etc/containers/policy.nix { };
 }
