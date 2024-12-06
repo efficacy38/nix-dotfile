ca952666a67fe0420b5834fb617f7b0a7e52d7a3
efficacy38 <efficacy38@gmail.com>
2024-12-06 21:45:39 +0800

    feat(k8s): add gcp k8s auth plugins

--- home-modules/ops/k8s.nix
+++ home-modules/ops/k8s.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  gdk = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
+    gke-gcloud-auth-plugin
+  ]);
+in
+{
   home.packages = with pkgs; [
     fluxcd
     kubectl
@@ -21,5 +27,9 @@
     kubectl-validate
     kube-prompt
     kyverno
+
+    # google cloud
+    gdk
+    terraform
   ];
 }
