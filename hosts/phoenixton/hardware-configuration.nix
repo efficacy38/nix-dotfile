2748095a2b3f0ec0c60c0091acbebd744df5f3a9
efficacy38 <efficacy38@gmail.com>
2025-01-17 05:11:29 +0800

    feat(nix): add nix-impermanent to laptop

--- hosts/phoenixton/hardware-configuration.nix
+++ hosts/phoenixton/hardware-configuration.nix
@@ -86,30 +86,43 @@
     };
   };
 
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/disk/by-label/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/@ ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/@ "/btrfs_tmp/old_roots/$timestamp"
-    fi
+  boot.initrd.systemd = {
+    enable = true;
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+    services.create-empty-btrfs-subvdir = {
+      wantedBy = [ "initrd-root-device.target" ];
+      after = [
+        "sysroot.mount"
+        "local-fs-pre.target"
+      ];
+      serviceConfig.Type = "oneshot";
+      unitConfig.DefaultDependencies = false;
+      script = ''
+        mkdir /btrfs_tmp
+        mount /dev/disk/by-label/root /btrfs_tmp
+        if [[ -e /btrfs_tmp/@ ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/@ "/btrfs_tmp/old_roots/$timestamp"
+        fi
+
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
         done
-        btrfs subvolume delete "$1"
-    }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/@
-    umount /btrfs_tmp
-  '';
+        btrfs subvolume create /btrfs_tmp/@
+        umount /btrfs_tmp
+      '';
+    };
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/181a096d-2934-425f-ac9b-73095e8678fa";
