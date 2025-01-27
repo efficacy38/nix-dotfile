2b38e014c07562f57e6a455ea0a5388c3a7ef3c6
efficacy38 <efficacy38@gmail.com>
2025-01-27 20:06:50 +0800

    rollback(nix): rollback pulseaudio for backward capacity

--- modules/my-desktop.nix
+++ modules/my-desktop.nix
@@ -40,7 +40,7 @@ in
     };
 
     # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
+    hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
