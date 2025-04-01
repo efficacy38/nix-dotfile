3c797aff53bcacf343fabb4d045768c96c5c4299
efficacy38 <efficacy38@gmail.com>
2025-04-01 22:21:33 +0800

    feat(impremanence): move zsh history to prevent bug

--- home-modules/features/impremanence.nix
+++ home-modules/features/impremanence.nix
@@ -18,8 +18,15 @@ in
       ".gnupg"
       ".ssh"
       ".nixops"
+      # TODO: tmporary add .local/share, we should add data when using some
+      # application
       ".local/share"
+      # ".local/share/rime"
+      # ".local/share/zsh"
+      ".config/Moonlight\ Game\ Streaming\ Project"
+      ".local/state"
       ".config/rambox"
+      ".config/solaar"
       ".krew"
       ".kube"
       ".mozilla"
@@ -28,8 +35,12 @@ in
       (symDirectory ".local/share/Steam")
     ];
     allowOther = true;
+
     files = [
-      ".zsh_history"
     ];
   };
+
+  # NOTICE: workaround of nix impermanence
+  # ref: https://github.com/nix-community/impermanence/issues/233
+  programs.zsh.history.path = "$HOME/.local/share/zsh/.zsh_history";
 }
