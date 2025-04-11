58a78a8b0190b923098840f9973d3c51fa98dcf7
efficacy38 <efficacy38@gmail.com>
2025-04-11 18:36:33 +0800

    feat(impremanence): add more impremanence config

--- home-modules/features/impremanence.nix
+++ home-modules/features/impremanence.nix
@@ -17,17 +17,37 @@ in
         ".nixops"
         # TODO: tmporary add .local/share, we should add data when using some
         # application
-        ".local/share"
-        # ".local/share/rime"
-        # ".local/share/zsh"
-        ".config/Moonlight\ Game\ Streaming\ Project"
+        # ".local/share"
+        ".local/share/rime"
+        ".local/share/zsh"
+        ".local/share/direnv"
+        ".local/share/dolphin"
+        ".local/share/fcitx5"
+        ".local/share/fish"
+        ".local/share/kate"
+        ".local/share/k9s"
+        ".local/share/mc"
+        ".local/share/nvim"
+        ".local/share/pnpm"
+        ".local/share/podman"
+        ".local/share/PrismLauncher"
+        ".local/share/remmina"
+        ".local/share/sddm"
+        ".local/share/tldr"
+        ".local/share/Trash"
+        ".local/share/yarn"
+        ".local/share/zoxide"
+        ".local/share/zsh"
         ".local/state"
+
+        ".config/Moonlight\ Game\ Streaming\ Project"
         ".config/rambox"
         ".config/solaar"
         ".krew"
         ".kube"
         ".mozilla"
         ".thunderbird"
+        ".config/fcitx5"
       ]
       ++ (map symDirectory [
         "Music"
@@ -38,6 +58,7 @@ in
         "Videos"
         "Sync"
         ".local/share/Steam"
+        ".local/share/containers"
       ]);
 
     files = [
