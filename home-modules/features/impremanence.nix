c33f986b19834b779c56d47064df8e0cc7944421
efficacy38 <efficacy38@gmail.com>
2025-04-27 13:40:39 +0800

    feat(impremanence): add missing directory

--- home-modules/features/impremanence.nix
+++ home-modules/features/impremanence.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 let
   symDirectory = dir: {
     directory = dir;
@@ -14,40 +14,17 @@ in
         "Postman"
         ".gnupg"
         ".ssh"
+        ".mozilla"
         ".nixops"
-        # TODO: tmporary add .local/share, we should add data when using some
-        # application
-        # ".local/share"
-        ".local/share/rime"
-        ".local/share/zsh"
-        ".local/share/direnv"
-        ".local/share/dolphin"
-        ".local/share/fcitx5"
-        ".local/share/fish"
-        ".local/share/kate"
-        ".local/share/k9s"
-        ".local/share/mc"
-        ".local/share/nvim"
-        ".local/share/pnpm"
-        ".local/share/podman"
-        ".local/share/PrismLauncher"
-        ".local/share/remmina"
-        ".local/share/sddm"
-        ".local/share/tldr"
-        ".local/share/Trash"
-        ".local/share/yarn"
-        ".local/share/zoxide"
-        ".local/share/zsh"
         ".local/state"
 
         ".config/Moonlight\ Game\ Streaming\ Project"
         ".config/rambox"
         ".config/solaar"
-        ".krew"
-        ".kube"
-        ".mozilla"
-        ".thunderbird"
-        ".config/fcitx5"
+
+        ".local/share/fcitx5"
+        ".local/share/zoxide"
+        ".local/share/zsh"
       ]
       ++ (map symDirectory [
         "Music"
@@ -58,13 +35,40 @@ in
         "Videos"
         "Sync"
         "Zotero"
+
+        ".config/fcitx5"
+        ".krew"
+        ".kube"
+
+        ".local/share/PrismLauncher"
         ".local/share/Steam"
+        ".local/share/Trash"
         ".local/share/containers"
+        ".local/share/direnv"
+        ".local/share/dolphin"
+        ".local/share/fish"
+        ".local/share/kate"
+        ".local/share/k9s"
+        ".local/share/mc"
+        ".local/share/nvim"
+        ".local/share/pnpm"
+        ".local/share/podman"
+        ".local/share/sddm"
+        ".local/share/tldr"
+        ".local/share/rime"
+        ".local/share/remmina"
+        ".local/share/yarn"
+        ".mc"
+        ".thunderbird"
       ]);
 
     files = [
     ];
   };
+
+  home.activation.fixPathForImpermanence = lib.hm.dag.entryBefore [ "cleanEmptyLinkTargets" ] ''
+    PATH=$PATH:/run/wrappers/bin
+  '';
 
   # NOTICE: workaround of nix impermanence
   # ref: https://github.com/nix-community/impermanence/issues/233
