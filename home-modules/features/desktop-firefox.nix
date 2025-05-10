024f7027cc5a4a5e6202330b091687e590b6446f
efficacy38 <efficacy38@gmail.com>
2025-05-11 06:44:04 +0800

    refactor(firefox): use extension config to arrange extension

--- home-modules/features/desktop-firefox.nix
+++ home-modules/features/desktop-firefox.nix
@@ -21,36 +21,26 @@
       ExtensionSettings =
         let
           navExtIds = [
-            "keepassxc-browser@keepassxc.org"
-            "addon@darkreader.org"
             # rabby wallet
             "{743634b5-5b80-4035-a92b-e6aac3fc32ed}"
+            "keepassxc-browser@keepassxc.org"
             "simple-tab-groups@drive4ik"
-            # protonpass
-            "{873e3fcc-d6d8-40e1-9606-80f6f5590887}"
-            # zotero connector
-            "zotero@chnm.gmu.edu"
+            "addon@darkreader.org"
           ];
 
           menuExtIds = [
             "uBlock0@raymondhill.net"
             "sponsorBlocker@ajay.app"
-            # Vimium
-            "{d7742d87-e61d-4b78-b8a1-b469842139fa}"
-            # multi account
-            "@testpilot-containers"
-            # yourutbe short block
-            "{34daeb50-c2d2-4f14-886a-7160b24d66a4}"
           ];
 
           posExtNav = extId: {
-            ${extId} = {
+            extId = {
               default_area = "navbar";
             };
           };
 
           posExtMenu = extId: {
-            "${extId}" = {
+            extId = {
               default_area = "menupanel";
             };
           };
@@ -65,6 +55,7 @@
     profiles.personal = {
       extensions = with inputs.firefox-addons.packages."${pkgs.system}"; [
         keepassxc-browser
+        ublock-origin
         sponsorblock
         darkreader
         vimium
@@ -72,8 +63,6 @@
         youtube-shorts-block
         simple-tab-groups
         zotero-connector
-        proton-pass
-        ublock-origin
       ];
 
       search.engines = {
@@ -114,7 +103,7 @@
 
         # taken from Misterio77's config
         "browser.uiCustomization.state" =
-          ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
+          ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
         "identity.fxaccounts.enabled" = false;
         "privacy.trackingprotection.enabled" = true;
         "signon.rememberSignons" = false;
