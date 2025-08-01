c7374331f90255407e6efcac071f2b481552e30c
efficacy38 <efficacy38@gmail.com>
2025-08-02 00:30:32 +0800

    chore: reformant flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -5,20 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-    };
-
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     sops-nix.url = "github:Mic92/sops-nix";
-
+    impermanence.url = "github:nix-community/impermanence";
+    stylix.url = "github:danth/stylix/release-25.05";
+    zen-browser.url = "github:youwen5/zen-browser-flake";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     nix-secrets = {
       url = "git+ssh://git@github.com/efficacy38/nix-secret.git?ref=main&shallow=1";
       # url = "git+ssh://git@gitlab.com/emergentmind/nix-secrets.git?ref=main&shallow=1";
@@ -26,31 +18,23 @@
       flake = false;
     };
 
-    solaar = {
-      url = "github:Svenum/Solaar-Flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    efficacy38-nur = {
-      url = "github:efficacy38/nur-packages";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager-stable.url = "github:nix-community/home-manager/release-25.05";
+    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs";
 
-    impermanence.url = "github:nix-community/impermanence";
+    solaar.url = "github:Svenum/Solaar-Flake";
+    solaar.inputs.nixpkgs.follows = "nixpkgs";
 
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    efficacy38-nur.url = "github:efficacy38/nur-packages";
+    efficacy38-nur.inputs.nixpkgs.follows = "nixpkgs";
+
+    firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    stylix.url = "github:danth/stylix/release-25.05";
-
-    zen-browser.url = "github:youwen5/zen-browser-flake";
-
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
   };
 
   outputs =
