e32911586428ffc16b4e98d6102f6f048072af8b
efficacy38 <efficacy38@gmail.com>
2026-01-23 00:41:39 +0800

    feat: update claude settings

--- flake-modules/modules/features/devpack/shell.nix
+++ flake-modules/modules/features/devpack/shell.nix
@@ -22,7 +22,7 @@ _: {
         programs = {
           zoxide = {
             enable = true;
-            options = [ "--cmd cd" ];
+            # options = [ "--cmd cd" ];
             enableZshIntegration = true;
           };
 
